cc264999106a9e5a7872dead3a40fa79fa4f0b51
JoseiToAoiTori <acciowand200@hotmail.com>
2020-08-22 12:58:45 +0300

    charts weeeee

---
+++ general/activity.js
@@ -0,0 +1,97 @@
+const {Command} = require('yuuko');
+const superagent = require('superagent');
+const distinctColors = require('distinct-colors').default
+
+let config;
+
+try {
+	config = require('../config');
+} catch (error) {
+	config = {};
+}
+
+module.exports = new Command('activity', async (incomingMessage, args, {yuuko}) => {
+	const owner = config.owner || process.env.OWNER;
+	if (args.length > 0 && incomingMessage.author.id === owner) {
+		const channel = await yuuko.getChannel(args[0]);
+		const msg = await incomingMessage.channel.createMessage({
+			embed: {
+				title: 'Generating chart',
+				color: config.colour || process.env.COLOUR,
+			},
+		});
+		const messages = await channel.getMessages(Number.MAX_SAFE_INTEGER);
+		console.log('Messages acquired');
+		const users = new Set(messages.map(message => message.author.username));
+		const colours = distinctColors({count: users.size});
+		const dataset = [];
+		let index = 0;
+		for (const user of users) {
+			const data = {
+				label: user,
+				borderColor: colours[index].css(),
+				fill: false,
+				data: [],
+			};
+			index++;
+			const userMessages = messages.filter(message => message.author.username === user).sort((a, b) => a.timestamp - b.timestamp);
+			let startTime = userMessages[0].timestamp; // Start time for plotting
+			while (startTime <= userMessages[userMessages.length - 1].timestamp) {
+				data.data.push({
+					x: new Date(startTime),
+					y: userMessages.filter(message => message.timestamp > startTime && message.timestamp < startTime + 86400000).length,
+				});
+				startTime += 86400000;
+			}
+			dataset.push(data);
+		}
+		const chart = {
+			type: 'line',
+			data: {datasets: dataset},
+			options: {
+				scales: {
+					xAxes: [{
+						type: 'time',
+						time: {
+							unit: 'day',
+						},
+					}],
+				},
+			},
+		};
+		let response;
+		try {
+			response = await superagent
+				.post('https://quickchart.io/chart/create')
+				.send({
+					backgroundColor: 'transparent',
+					width: 1500,
+					height: 900,
+					format: 'png',
+					chart,
+				});
+			msg.delete();
+			incomingMessage.channel.createMessage({
+				embed: {
+					title: `Chart Generated At: ${response.body.url}`,
+					color: config.colour || process.env.COLOUR,
+				},
+			});
+		} catch (error) {
+			msg.delete();
+			incomingMessage.channel.createMessage({
+				embed: {
+					title: 'Something went wrong.',
+					color: config.colour || process.env.COLOUR,
+				},
+			});
+		}
+	} else {
+		incomingMessage.channel.createMessage({
+			embed: {
+				title: "You can't do that.",
+				color: config.colour || process.env.COLOUR,
+			},
+		});
+	}
+});
