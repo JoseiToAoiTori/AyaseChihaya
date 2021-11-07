993853622c685d65942a23c05b39edbc0af962ca
JoseiToAoiTori <acciowand200@hotmail.com>
2021-11-07 10:05:10 +0200

    fix activity chart for hosts

--- general/activity.js
+++ general/activity.js
@@ -12,7 +12,7 @@ try {
 
 module.exports = new Command('activity', async (incomingMessage, args, {yuuko}) => {
 	const owner = config.owner || process.env.OWNER;
-	if (args.length > 0 && (incomingMessage.author.id === owner || (member.roles.cache.some(role => role.name === 'Host') && member.permissions.has(Permissions.FLAGS.MANAGE_CHANNELS)))) {
+	if (args.length > 0 && incomingMessage.author.id === owner || (incomingMessage.member.roles.some(role => role === '874337528990302268') && incomingMessage.guildID === '874337058192228403')) {
 		const channel = await yuuko.getChannel(args[0]);
 		const msg = await incomingMessage.channel.createMessage({
 			embed: {
--- general/character.js
+++ general/character.js
@@ -10,30 +10,30 @@ try {
 }
 
 module.exports = new Command('character', async (message, args) => {
-	if (args.length < 3) {
-		message.channel.createMessage({
-			embed: {
-				title: 'Please specify a character and server name in the format ;character <FIRSTNAME> <LASTNAME> <SERVER>.',
-				color: config.colour || process.env.COLOUR,
-			},
-		});
-		return;
-	}
-	let response;
-	try {
-		response = await superagent.get(`https://xiv-api-chihaya.herokuapp.com/characters/name/${args[2]}/${args[0]}%20${args[1]}.png`);
-	} catch (error) {
-		response = null;
-	}
-	if (response && response.ok && Buffer.isBuffer(response.body)) {
-		console.log(response.body);
-		message.channel.createMessage('Praise Yoshi-P', {file: response.body, name: `${args[0]}_${args[1]}.png`});
-	} else {
-		message.channel.createMessage({
-			embed: {
-				title: 'Help Yoshi-P is banging on my door, yelling at me to buy the new mogstation mount!!!!!',
-				color: config.colour || process.env.COLOUR,
-			},
-		});
-	}
+	// if (args.length < 3) {
+	//	message.channel.createMessage({
+	//		embed: {
+	//			title: 'Please specify a character and server name in the format ;character <FIRSTNAME> <LASTNAME> <SERVER>.',
+	//			color: config.colour || process.env.COLOUR,
+	//		},
+	//	});
+	//	return;
+	// }
+	// let response;
+	// try {
+	//	response = await superagent.get(`https://xiv-api-chihaya.herokuapp.com/characters/name/${args[2]}/${args[0]}%20${args[1]}.png`);
+	// } catch (error) {
+	//	response = null;
+	// }
+	// if (response && response.ok && Buffer.isBuffer(response.body)) {
+	//	console.log(response.body);
+	//	message.channel.createMessage('Praise Yoshi-P', {file: response.body, name: `${args[0]}_${args[1]}.png`});
+	// } else {
+	//	message.channel.createMessage({
+	//		embed: {
+	//			title: 'Help Yoshi-P is banging on my door, yelling at me to buy the new mogstation mount!!!!!',
+	//			color: config.colour || process.env.COLOUR,
+	//		},
+	//	});
+	// }
 });
