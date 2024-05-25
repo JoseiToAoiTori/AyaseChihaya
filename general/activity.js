9d491cf9871737c55933354ec7d2cf981d20c114
JoseiToAoiTori <acciowand200@hotmail.com>
2024-05-26 00:47:56 +0200

    rework chart into pie

--- general/activity.js
+++ general/activity.js
@@ -1,6 +1,5 @@
 const {Command} = require('yuuko');
-const superagent = require('superagent');
-const distinctColors = require('distinct-colors').default;
+const shortUrl = require("node-url-shortener");
 
 let config;
 
@@ -12,7 +11,7 @@ try {
 
 module.exports = new Command('activity', async (incomingMessage, args, {yuuko}) => {
 	const owner = config.owner || process.env.OWNER;
-	if (args.length > 0 && incomingMessage.author.id === owner || (incomingMessage.member.roles.some(role => role === '874337528990302268') && incomingMessage.guildID === '874337058192228403')) {
+	if (args.length > 0 && incomingMessage.author.id === owner) {
 		const channel = await yuuko.getChannel(args[0]);
 		const msg = await incomingMessage.channel.createMessage({
 			embed: {
@@ -23,77 +22,31 @@ module.exports = new Command('activity', async (incomingMessage, args, {yuuko}) 
 		const messages = await channel.getMessages(20000);
 		console.log('Messages acquired');
 		const users = new Set(messages.map(message => message.author.username));
-		const colours = distinctColors({
-			count: users.size,
-			samples: 3200,
-			quality: 200,
-		});
-		const dataset = [];
-		let index = 0;
+		const dataset = [{
+			data: [],
+		}];
+		const labels = [];
 		for (const user of users) {
-			const data = {
-				label: user,
-				borderColor: colours[index].css(),
-				fill: false,
-				data: [],
-			};
-			index++;
-			const userMessages = messages.filter(message => message.author.username === user && message.content.split(' ').length > 2).sort((a, b) => a.timestamp - b.timestamp);
+			const userMessages = messages.filter(message => message.author.username === user);
 			if (userMessages.length === 0) continue;
-			let startTime = userMessages[0].timestamp; // Start time for plotting
-			while (startTime <= userMessages[userMessages.length - 1].timestamp) {
-				const wordCount = userMessages.filter(message => message.timestamp > startTime && message.timestamp < startTime + 86400000).reduce((accumulator, message) => accumulator + message.content.split(' ').length, 0);
-				if (wordCount > 50) {
-					data.data.push({
-						x: new Date(startTime),
-						y: wordCount,
-					});
-				}
-				startTime += 86400000;
-			}
-			dataset.push(data);
+			labels.push(user);
+			dataset[0].data.push(userMessages.length);
 		}
 		const chart = {
-			type: 'line',
-			data: {datasets: dataset},
-			options: {
-				scales: {
-					xAxes: [{
-						type: 'time',
-						time: {
-							unit: 'day',
-						},
-					}],
-				},
-			},
+			type: 'pie',
+			data: {datasets: dataset, labels},
 		};
-		let response;
-		try {
-			response = await superagent
-				.post('https://quickchart.io/chart/create')
-				.send({
-					backgroundColor: 'transparent',
-					width: 1500,
-					height: 900,
-					format: 'png',
-					chart,
-				});
+		let url = `https://quickchart.io/chart?c=${encodeURIComponent(JSON.stringify(chart))}`;
+		console.log(JSON.stringify(chart));
+		shortUrl.short(url, function (err, url) {
 			msg.delete();
 			incomingMessage.channel.createMessage({
 				embed: {
-					title: `Chart Generated At: ${response.body.url}`,
+					title: `Chart Generated At: ${url}`,
 					color: config.colour || process.env.COLOUR,
 				},
 			});
-		} catch (error) {
-			msg.delete();
-			incomingMessage.channel.createMessage({
-				embed: {
-					title: 'Something went wrong.',
-					color: config.colour || process.env.COLOUR,
-				},
-			});
-		}
+		});
 	} else {
 		incomingMessage.channel.createMessage({
 			embed: {
