1b210190fda7c72700d387032efdabb8c5f88f88
JoseiToAoiTori <acciowand200@hotmail.com>
2024-05-26 01:58:00 +0200

    aaaaaaaaaaah

--- general/activity.js
+++ general/activity.js
@@ -23,37 +23,44 @@ module.exports = new Command('activity', async (incomingMessage, args, {yuuko}) 
 		const messages = await channel.getMessages(20000);
 		console.log('Messages acquired');
 		const users = new Set(messages.map(message => message.author.username));
-		const palette = iwanthue(users.size)
+		const palette = iwanthue(users.size);
 		const dataset = [{
 			data: [],
-			backgroundColor: palette
+			backgroundColor: palette,
 		}];
+		const posters = [];
 		const labels = [];
 		for (const user of users) {
 			const userMessages = messages.filter(message => message.author.username === user);
 			if (userMessages.length === 0) continue;
-			labels.push(user);
-			dataset[0].data.push(userMessages.length);
+			posters.push({
+				user,
+				count: userMessages.length,
+			});
 		}
-		dataset[0].data.sort((a, b) => a - b);
+		posters.sort((a, b) => b.count - a.count);
+		for (const poster of posters) {
+			labels.push(poster.user);
+			dataset[0].data.push(poster.count);
+		}
 		const chart = {
 			type: 'outlabeledPie',
 			data: {datasets: dataset, labels},
-			"options": {
-				"plugins": {
-				  "legend": false,
-				  "outlabels": {
-					"text": "%l %p",
-					"color": "white",
-					"stretch": 30,
-					"font": {
-					  "resizable": true,
-					  "minSize": 12,
-					  "maxSize": 18
-					}
-				  }
-				}
-			}
+			options: {
+				plugins: {
+					legend: false,
+					outlabels: {
+						text: '%l %p',
+						color: 'white',
+						stretch: 30,
+						font: {
+							resizable: true,
+							minSize: 12,
+							maxSize: 18,
+						},
+					},
+				},
+			},
 		};
 		let url = `https://quickchart.io/chart?c=${encodeURIComponent(JSON.stringify(chart))}`;
 		console.log(JSON.stringify(chart));
