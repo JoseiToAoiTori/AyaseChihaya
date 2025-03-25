378da81af869e6b5b2700d38ecc5e7c413391636
JoseiToAoiTori <acciowand200@hotmail.com>
2025-03-25 22:23:17 +0100

    update for new season

--- next-cache.js
+++ next-cache.js
@@ -36,8 +36,8 @@ async function getSeasonalShows () {
 	// FALL
 	// Months September to November
 	//
-	const startdateGreater = 20241201;
-	const startdateLesser = 20250230;
+	const startdateGreater = 20240301;
+	const startdateLesser = 20250530;
 	try {
 		data = await superagent
 			.post('https://graphql.anilist.co')
